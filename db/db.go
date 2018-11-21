e1a35be6c077c670a891df80214d4821344e50b0
Tomoya AMACHI <tomoya.amachi@gmail.com>
2018-11-22 00:06:22 +0900

    add Database connector

---
+++ db/db.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"fmt"
+	"github.com/inconshreveable/log15"
+	"github.com/tomoyamachi/gocarts/models"
+)
+
+// DB is interface for a database driver
+type DB interface {
+	Name() string
+	OpenDB(string, string, bool) (bool, error)
+	MigrateDB() error
+	InsertJpcert([]models.JpcertArticle) error
+}
+
+// NewDB returns db driver
+func NewDB(dbType, dbPath string, debugSQL bool) (driver DB, locked bool, err error) {
+	if driver, err = newDB(dbType); err != nil {
+		log15.Error("Failed to new db.", "err", err)
+		return driver, false, err
+	}
+
+	log15.Info("Opening DB.", "db", driver.Name())
+	if locked, err := driver.OpenDB(dbType, dbPath, debugSQL); err != nil {
+		if locked {
+			return nil, true, err
+		}
+		return nil, false, err
+	}
+
+	log15.Info("Migrating DB.", "db", driver.Name())
+	if err := driver.MigrateDB(); err != nil {
+		log15.Error("Failed to migrate db.", "err", err)
+		return driver, false, err
+	}
+	return driver, false, nil
+}
+
+func newDB(dbType string) (DB, error) {
+	switch dbType {
+	case dialectSqlite3, dialectMysql, dialectPostgreSQL:
+		return &RDBDriver{name: dbType}, nil
+		//case dialectRedis:
+		//	return &RedisDriver{name: dbType}, nil
+	}
+	return nil, fmt.Errorf("Invalid database dialect. err: %s", dbType)
+}
---
+++ db/jpcert.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"fmt"
+	"github.com/inconshreveable/log15"
+	"github.com/jinzhu/gorm"
+	"github.com/tomoyamachi/gocarts/models"
+	pb "gopkg.in/cheggaaa/pb.v1"
+)
+
+func (r *RDBDriver) deleteAndInsertJpcert(conn *gorm.DB, article models.JpcertArticle) (err error) {
+	tx := conn.Begin()
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		tx.Commit()
+	}()
+
+	// Delete old records if found
+	old := models.JpcertArticle{}
+	result := tx.Where(&models.JpcertArticle{ArticleID: article.ArticleID}).First(&old)
+	if !result.RecordNotFound() {
+		// Delete old records
+		var errs gorm.Errors
+		errs = errs.Add(tx.Unscoped().Delete(&old).Error)
+
+		// Delete nil in errs
+		var validErrs []error
+		for _, err := range errs {
+			if err != nil {
+				validErrs = append(validErrs, err)
+			}
+		}
+		errs = validErrs
+
+		if len(errs.GetErrors()) > 0 {
+			return fmt.Errorf("Failed to delete old records. id: %s, err: %s",
+				article.ArticleID, errs.Error())
+		}
+	}
+	if err = tx.Create(&article).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *RDBDriver) InsertJpcert(articles []models.JpcertArticle) (err error) {
+	bar := pb.StartNew(len(articles))
+	log15.Info(fmt.Sprintf("Insert %d articles", len(articles)))
+	for _, article := range articles {
+		if err := r.deleteAndInsertJpcert(r.conn, article); err != nil {
+			return fmt.Errorf("Failed to insert. article: %s, err: %s",
+				article.ArticleID, err)
+		}
+		bar.Increment()
+	}
+	bar.Finish()
+	return nil
+}
